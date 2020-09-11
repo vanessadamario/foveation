bbf66359cb3dc792ea4056b3adf6d766d664dea6
Unknown <vanedamario@gmail.com>
2020-09-10 21:46:46 -0400

    ready for dgx

--- ImageNet_dogs_framework/tensorflow_models/official/resnet/launch_pilot.sh
+++ ImageNet_dogs_framework/tensorflow_models/official/resnet/launch_pilot.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --array=0
-#SBATCH -c 2
+#SBATCH -c 4
 #SBATCH --job-name=resnet
-#SBATCH --gres=gpu:tesla-k80:2
-#SBATCH --mem=20GB
-#SBATCH -t 00:30:00
-#SBATCH --partition=cbmm
+#SBATCH --gres=gpu:tesla-k80:4
+#SBATCH --mem=30GB
+#SBATCH -t 01:00:00
+#SBATCH --qos=cbmm
 
-module add openmind/singularity/3.4.1
 export PYTHONPATH="$PYTHONPATH:/om/user/vanessad/ImageNet_dogs_framework/tensorflow_models/official/resnet"
 
 singularity exec -B /om:/om --nv /om/user/xboix/singularity/xboix-tensorflow1.14.simg \
 python imagenet_main.py  \
---data_dir=/om/user/vanessad/ImageNet_dogs_framework/TFRecords \
---num_gpus=2 \
---batch_size=64 \
+--data_dir= /raid/poggio/home/xboix/data/imagenet-tfrecords\
+--num_gpus=4 \
+--batch_size=128 \
 --train_epochs=20 \
 --model_dir=/om/user/vanessad/ImageNet_dogs_framework/tensorflow_models/official/resnet/pilot_cropped
 
