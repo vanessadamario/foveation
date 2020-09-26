01db483fa4db458bc1db37c3a330371d797375ea
Unknown <vanedamario@gmail.com>
2020-09-25 21:51:06 -0400

    include flag
    crop_image=True

--- ImageNet_dogs_framework/tensorflow_models/official/utils/flags/_base.py
+++ ImageNet_dogs_framework/tensorflow_models/official/utils/flags/_base.py
@@ -28,7 +28,7 @@ from utils.flags._conventions import help_wrap
 from utils.logs import hooks_helper
 
 
-def define_base(crop_image=0, data_dir=True, model_dir=True, clean=True, train_epochs=True,
+def define_base(crop_image=True, data_dir=True, model_dir=True, clean=True, train_epochs=True,
                 epochs_between_evals=True, stop_threshold=True, batch_size=True,
                 num_gpu=True, hooks=True, export_dir=True):
   """Register base flags.
